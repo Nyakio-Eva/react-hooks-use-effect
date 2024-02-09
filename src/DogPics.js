90e8da6530afef3b25e19b4584c3d089f3fa02d5
Nyakio-Eva <evalkariuki@gmail.com>
2024-02-09 21:53:27 +0300

    completed assignment

--- src/DogPics.js
+++ src/DogPics.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-// WARNING: this useEffect will run in an infinite loop!
-// to fix, pass an empty array as the second argument for useEffect
+
 function DogPics() {
   const [images, setImages] = useState([]);
 
@@ -13,14 +12,14 @@ function DogPics() {
         console.log("setState");
         setImages(data.message);
       });
-  });
+    }, []);
 
   console.log("render");
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt="Dog pic"/>
       ))}
     </div>
   );
